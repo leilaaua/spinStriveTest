224f51661b24c4c3dd9e13c76cefbc324ceec0b2
leila <leila0777@icloud.com>
2024-04-24 13:41:57 +0400

    Initial Commit

---
+++ SpinStriveTest/SpinStriveTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpinStriveTestApp.swift
+//  SpinStriveTest
+//
+//  Created by leila on 24.04.24.
+//
+
+import SwiftUI
+
+@main
+struct SpinStriveTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
